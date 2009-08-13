7ec788ae4709609baccd2a3cad229431f33fd535
larsgeorge <lars.george@gmail.com>
2009-08-13 16:41:28 +0200

    Fixed build.xml jar creation and start script.

--- bin/hbase-manager.sh
+++ bin/hbase-manager.sh
@@ -1,14 +1,17 @@
+#!/bin/bash
+
 # set main class
 MAIN_CLASS="com.larsgeorge.hadoop.hbase.HBaseManager"
 
 # determine where we are
-TOOL_HOME="$(dirname $0)"
+TOOL_HOME=$(dirname "$0")
+TOOL_HOME=$(cd "$TOOL_HOME"; pwd)
 
 # set up classpath
-for f in $TOOL_HOME/*.jar; do
+for f in $TOOL_HOME/../*.jar; do
   CLASSPATH=${CLASSPATH}:$f
 done
-for f in $TOOL_HOME/lib/*.jar; do
+for f in $TOOL_HOME/../lib/*.jar; do
   CLASSPATH=${CLASSPATH}:$f
 done
 echo "using classpath: $CLASSPATH"
